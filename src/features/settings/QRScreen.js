cb94e8f8ca48f0cf9d26eda04eb5ea6319e635d4
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2019-03-01 20:19:27 +0700

    update qr code scanner screen

--- src/features/settings/QRScreen.js
+++ src/features/settings/QRScreen.js
@@ -1,55 +1,50 @@
 import React, { Component } from 'react'
-
-import { StyleSheet, Text, TouchableOpacity, Linking } from 'react-native'
-
-import QRCodeScanner from 'react-native-qrcode-scanner'
+import { View, StyleSheet, Text, TouchableOpacity, Linking } from 'react-native'
+import ScannerScreen from './ScannerScreen'
+import { withNavigationFocus } from 'react-navigation'
+import { Button } from 'react-native-elements'
 
 class ScanScreen extends Component {
-  onSuccess(e) {
-    console.log('e: ', e)
-    // Linking
-    //   .openURL(e.data)
-    //   .catch(err => console.error('An error occured', err));
+  state = {
+    shouldShowCamera: false
+  }
+
+  componentDidUpdate = (prevProps, prevState) => {
+    console.log('show: ', this.props.isFocused)
+  }
+
+  renderCamera() {
+    const isFocused = this.props.navigation.isFocused()
+    const { shouldShowCamera } = this.state
+    if (shouldShowCamera && isFocused) {
+      return <ScannerScreen />
+    } else {
+      return null
+    }
+  }
+
+  handleShowCamera = () => {
+    this.setState({ shouldShowCamera: !this.state.shouldShowCamera })
   }
 
   render() {
+    const {
+      handleShowCamera,
+      state: { shouldShowCamera }
+    } = this
+
     return (
-      <QRCodeScanner
-        onRead={this.onSuccess.bind(this)}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer
-            and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, padding: 12 }}>
+          <Button
+            title={shouldShowCamera ? 'Turn off camera' : 'Show camera'}
+            onPress={handleShowCamera}
+          />
+        </View>
+        <View style={{ flex: 3 }}>{this.renderCamera()}</View>
+      </View>
     )
   }
 }
 
-export default ScanScreen
-
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000'
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
-  },
-  buttonTouchable: {
-    padding: 16
-  }
-})
+export default withNavigationFocus(ScanScreen)
---
+++ src/features/settings/ScannerScreen.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+
+import { View, Dimensions, Text } from 'react-native'
+import QRCodeScanner from 'react-native-qrcode-scanner'
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
+
+console.disableYellowBox = true
+
+class QrCodeCamera extends Component {
+  onSuccess(e) {
+    console.log('e: ', e)
+  }
+
+  makeSlideOutTranslation(translationType, fromValue) {
+    return {
+      from: {
+        [translationType]: SCREEN_WIDTH * -0.18
+      },
+      to: {
+        [translationType]: fromValue
+      }
+    }
+  }
+
+  render() {
+    const { onSuccess } = this
+
+    return (
+      <QRCodeScanner
+        showMarker
+        onRead={onSuccess}
+        cameraStyle={{ height: SCREEN_HEIGHT }}
+        customMarker={
+          <View style={styles.rectangleContainer}>
+            <View style={styles.topOverlay}>
+              <Text style={{ fontSize: 30, color: 'white' }}>QR CODE SCANNER</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row' }}>
+              <View style={styles.leftAndRightOverlay} />
+
+              <View style={styles.rectangle} />
+
+              <View style={styles.leftAndRightOverlay} />
+            </View>
+
+            <View style={styles.bottomOverlay} />
+          </View>
+        }
+      />
+    )
+  }
+}
+
+const overlayColor = 'rgba(0,0,0,0.5)' // this gives us a black color with a 50% transparency
+
+const rectDimensions = SCREEN_WIDTH * 0.65 // this is equivalent to 255 from a 393 device width
+const rectBorderWidth = SCREEN_WIDTH * 0.005 // this is equivalent to 2 from a 393 device width
+const rectBorderColor = 'red'
+
+const scanBarWidth = SCREEN_WIDTH * 0.46 // this is equivalent to 180 from a 393 device width
+const scanBarHeight = SCREEN_WIDTH * 0.0025 //this is equivalent to 1 from a 393 device width
+const scanBarColor = '#22ff00'
+
+const iconScanColor = '#fff'
+
+const styles = {
+  rectangleContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+
+  rectangle: {
+    height: rectDimensions,
+    width: rectDimensions,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+
+  topOverlay: {
+    flex: 1,
+    height: SCREEN_WIDTH,
+    width: SCREEN_WIDTH,
+    backgroundColor: overlayColor,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  bottomOverlay: {
+    flex: 1,
+    height: SCREEN_WIDTH,
+    width: SCREEN_WIDTH,
+    backgroundColor: overlayColor,
+    paddingBottom: SCREEN_WIDTH * 0.25
+  },
+
+  leftAndRightOverlay: {
+    height: SCREEN_WIDTH * 0.65,
+    width: SCREEN_WIDTH,
+    backgroundColor: overlayColor
+  },
+
+  scanBar: {
+    width: scanBarWidth,
+    height: scanBarHeight,
+    backgroundColor: scanBarColor
+  }
+}
+
+export default QrCodeCamera
